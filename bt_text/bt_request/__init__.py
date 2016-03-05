afe78a80cbebfdcad11396ae7a952d0072afea82
Shawn Hanna <shawnkarlhanna@gmail.com>
2016-03-05 18:39:45 -0500

    added exception handling to bt python api

--- bt_text/bt_request/__init__.py
+++ bt_text/bt_request/__init__.py
@@ -1,85 +1,225 @@
 import xml.etree.ElementTree as ET
 import requests
+import pudb
 
+def __req_get_arrival_and_departure_times_for_trip(tripID):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetArrivalAndDepartureTimesForTrip", data={'tripID': tripID})
+    except requests.exceptions.RequestException as e:
+        exception = e
 
-def REQ_GetArrivalAndDepartureTimesForTrip(tripID):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetArrivalAndDepartureTimesForTrip", data={'tripID': tripID}).content
-    root = ET.fromstring(xml)
-    return root
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
 
-def REQ_GetCurrentBusInfo():
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetCurrentBusInfo").content
-    root = ET.fromstring(xml)
-    return root
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
 
-def REQ_GetCurrentRoutes(tripID):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetCurrentRoutes").content
-    root = ET.fromstring(xml)
-    return root
+def __req_get_current_bus_info():
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetCurrentBusInfo")
+    except requests.exceptions.RequestException as e:
+        exception = e
 
-def REQ_GetNextDepartures(routeShortName, stopCode):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetNextDepartures", data={'routeShortName': routeShortName, 'stopCode': stopCode}).content
-    root = ET.fromstring(xml)
-    return root
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
 
-def REQ_GetScheduledPatternPoints(patternName):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledPatternPoints", data={'patternName': patternName}).content
-    root = ET.fromstring(xml)
-    return root
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
 
-def REQ_GetScheduledRoutes(stopCode):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledRoutes", data={'stopCode': stopCode}).content
-    root = ET.fromstring(xml)
-    return root
+def __req_get_current_routes(tripID):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetCurrentRoutes")
+    except requests.exceptions.RequestException as e:
+        exception = e
 
-def REQ_GetScheduledStopCodes(stopCode):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledStopCodes", data={'routeShortName': routeShortName}).content
-    root = ET.fromstring(xml)
-    return root
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
 
-def REQ_GetScheduledStopNames(stopCode):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledStopNames", data={'routeShortName': routeShortName}).content
-    root = ET.fromstring(xml)
-    return root
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
 
-def REQ_GetSummary(stopCode):
-    xml = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetSummary").content
-    root = ET.fromstring(xml)
-    return root
+def __req_get_next_departures(routeShortName, stopCode):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetNextDepartures", data={'routeShortName': routeShortName, 'stopCode': stopCode})
+    except requests.exceptions.RequestException as e:
+        exception = e
 
-def GetNextDepartureTimesForRouteAndStopCode(routeShortName, stopCode, numTimesToReturn=3):
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def __req_get_scheduled_pattern_points(patternName):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledPatternPoints", data={'patternName': patternName})
+    except requests.exceptions.RequestException as e:
+        exception = e
+
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def __req_get_scheduled_routes(stopCode):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledRoutes", data={'stopCode': stopCode})
+    except requests.exceptions.RequestException as e:
+        exception = e
+
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def __req_get_scheduled_stop_codes(stopCode):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledStopCodes", data={'routeShortName': routeShortName})
+    except requests.exceptions.RequestException as e:
+        exception = e
+
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def __req_get_scheduled_stop_names(stopCode):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetScheduledStopNames", data={'routeShortName': routeShortName})
+    except requests.exceptions.RequestException as e:
+        exception = e
+
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def __req_get_summary(stopCode):
+    exception = None
+    try:
+        req = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetSummary")
+    except requests.exceptions.RequestException as e:
+        exception = e
+
+    if exception == None:
+        content = req.content
+        code = req.status_code
+        root = ET.fromstring(req.content)
+    else:
+        code = None
+        root = None
+        content = None
+
+    return {"status_code": code, "content": content, "xml": root, "exception": exception}
+
+def get_next_departure_times_for_route_and_stop_code(routeShortName, stopCode, numTimesToReturn=3):
     results = []
 
-    root = REQ_GetNextDepartures(routeShortName, stopCode)
-    for child in root.iter('AdjustedDepartureTime'):
-        results.append(child.text)
+    resp = __req_get_next_departures(routeShortName, stopCode)
+    success = True
+    if resp["status_code"] != None and resp["status_code"] == 200:
+        for child in resp["xml"].iter('AdjustedDepartureTime'):
+            results.append(child.text)
+        sorted(results)
+    else:
+        success = False
 
-    sorted(results)
-
-    return results[:numTimesToReturn]
-
+    return { "success": success, "status_code": resp["status_code"], "times": results[:numTimesToReturn]}
 
 def get_busses_for_stop_code(stopCode):
-    root = REQ_GetScheduledRoutes(stopCode)
+    resp = __req_get_scheduled_routes(stopCode)
 
-    results = []
+    route_short_names = []
+    route_names = []
 
-    for child in root.iter('RouteShortName'):
-        results.append(child.text)
+    # pudb.set_trace()
 
-    return results
+    success = True
+    if resp["status_code"] != None and resp["status_code"] == 200:
+        for child in resp["xml"].iter('RouteShortName'):
+            route_short_names.append(child.text)
+        for child in resp["xml"].iter('RouteName'):
+            route_names.append(child.text)
+    else:
+        success = False
+
+    return { "success": success, "status_code": resp["status_code"], "route_short_names": route_short_names, "route_names": route_names }
 
 
-def GetTimesForStopCode(stopCode):
-    routes = get_busses_for_stop_code(stopCode)
-
-    numTimesToReturn = 1
-
-    if len(routes) <= 2:
-        numTimesToReturn = 3
+def get_times_for_stop_code(stopCode):
+    busses_resp = get_busses_for_stop_code(stopCode)
 
     times = []
-    for route in routes:
-        times.append( (route, GetNextDepartureTimesForRouteAndStopCode(route, stopCode, numTimesToReturn) ) )
+    success = busses_resp["success"]
+    if busses_resp["status_code"] != None and busses_resp["status_code"] == 200 and success == True:
+        busses = busses_resp["route_short_names"]
 
-    return times
+        numTimesToReturn = 1
+
+        if len(busses) <= 2:
+            numTimesToReturn = 3
+
+        for route in busses:
+            next_deps = get_next_departure_times_for_route_and_stop_code(route, stopCode, numTimesToReturn)
+
+            if next_deps["status_code"] != None and next_deps["status_code"] == 200:
+                times.append( (route, next_deps["times"] ) )
+            else:
+                success = False
+                break
+    else:
+        success = False
+
+    return { "success": success, "times": times }
