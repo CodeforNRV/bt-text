21f4899fedf0c654c8384e6ae5ee855dbbadd0a1
Kevin Hill <kevin@pebble.com>
2016-07-07 20:19:12 -0400

    removed unneeded logging

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from bt_text import app
-import os
-
-PORT = int(os.environ.get('FLASK_PORT', 80))
-
 
 if __name__ == '__main__':
     app.debug = True
-    print("Listening on port {}".format(PORT))
     app.run(host='0.0.0.0', port=PORT)
