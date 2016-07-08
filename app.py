d37b80022d108813c527d4a0f2f29bc8d0f3468e
Kevin Hill <kevin@pebble.com>
2016-07-07 20:17:35 -0400

    added parsing for combined route codes and stop codes

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from bt_text import app
+import os
+
+PORT = int(os.environ.get('FLASK_PORT', 80))
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    print("Listening on port {}".format(PORT))
+    app.run(host='0.0.0.0', port=PORT)
